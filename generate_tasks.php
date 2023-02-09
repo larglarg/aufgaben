1e2152e88ae5dc35674e199222a6ba88e1588954
larglarg <81179677+larglarg@users.noreply.github.com>
2023-02-10 00:14:18 +0100

    Update generate_tasks.php

--- generate_tasks.php
+++ generate_tasks.php
@@ -1,84 +1,62 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "aufgaben");
-
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-}
+$conn = new PDO("mysql:host=localhost;dbname=aufgaben", "root", "");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $number_of_tasks = $_POST['number_of_tasks'];
 $difficulty_level = $_POST['difficulty_level'];
-$round = 0;
-for ($i = 0; $i < $number_of_tasks; $i++) {
-    $task = generateTask($difficulty_level, $round);
+$max_operationszahl = $_POST['max_operationszahl'];
+$min_operationszahl = $_POST['min_operationszahl'];
+$max_Start_Zahl = $_POST['max_Start_Zahl'];
+$min_Start_Zahl = $_POST['min_Start_Zahl'];
+$max_ergebniss = $_POST['max_groesse_ergebniss'];
 
-try {
-  $conn = new PDO("mysql:host=localhost;dbname=aufgaben", "root", "");
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO aufgaben (aufgabe, loesung, level, operation1, operation2)
-  VALUES (:value1, :value2, :value3, :value4, :value5)";
-  // Prepare statement
-  $stmt = $conn->prepare($sql);
-  // Bind parameters
-  $ergbnissfile = fopen("endergebnise.txt", "w");
-  $z = 1;
-  foreach ($task as $value) {
-    fwrite($ergbnissfile, $value."\n");
-    switch ($z) {
-        case 1:
-            $value1 = $value;
-        case 2:
-            $value2 = $value;
-        case 3: 
-            $value3 = $difficulty_level;
-        case 4:
-            $value4 = $value;
-        case 5:
-            $value5 = $value;
-    }
-    $z++;
-  
+for ($i = 0; $i < $number_of_tasks; $i++) {
+    // Erzeuge eine neue Aufgabe je nach Schwierigkeitsgrad
+    $task = generateTask($difficulty_level, $i, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss);
+
+
+   
+    // Füge die Daten in die Datenbank ein
+    $sql = "INSERT INTO aufgaben (aufgabe, loesung, level, operation1, operation2)
+    VALUES (:aufgabe, :loesung, :level, :operation1, :operation2)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':aufgabe', $task[0]);
+    $stmt->bindParam(':loesung', $task[1]);
+    $stmt->bindParam(':level', $task[2]);
+    $stmt->bindParam(':operation1', $task[3]);
+    $stmt->bindParam(':operation2', $task[4]);
+    $stmt->execute();
+
 }
-  $stmt->bindParam(':value1', $value1);
-  $stmt->bindParam(':value2', $value2);
-  $stmt->bindParam(':value3', $value3);
-  $stmt->bindParam(':value4', $value4);
-  $stmt->bindParam(':value5', $value5);
-  // Execute the query
-  $stmt->execute();
-  echo "New record created successfully";
-} catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
-}
-fclose($ergbnissfile);
+
 $conn = null;
 
 
-}
 
-function generateTask($difficulty_level, $roundtask) {
+function generateTask($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss) {
     switch ($difficulty_level) {
         case 1:
-            return generateLevel1Task($difficulty_level, $roundtask);
+            return generateLevel1Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss);
         case 2:
-            return generateLevel2Task();
+            return generateLevel2Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss);
         case 3:
             return generateLevel3Task();
-        default:
-            return generateLevel1Task();
     }
 }
 
-function generateLevel1Task($dif_lvl, $roundintern) {
-    $roundintern++;
-    $myfile = fopen("testfile".$roundintern.".txt", "w");
-    $sequence = array();
-    $numbber = rand(1, 100);
-    $operation1 = rand(-10, 10);
-    $operation2 = rand(-10, 10);
+function generateLevel1Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss) {
+    $round++;
+    $sequence = [];
+    $number = rand($min_start_zahl, $max_start_zahl);
+    $operation1 = rand($min_operationszahl, $max_operationszahl);
+    $operation2 = rand($min_operationszahl, $max_operationszahl);
+    $equal_different = rand(1, 2);
+    $verlche_version_vom_lvl = rand(1, 2);
+    if ($equal_different == 1){
+        $operation1 = $operation2;
+    }
     $length = rand(4, 6);
-    $y = 0; 
     if ($operation1 == 0) {
         $operation1 = 1;
     }
@@ -86,39 +64,105 @@ function generateLevel1Task($dif_lvl, $roundintern) {
         $operation2 = 1;
     }
     for ($i = $length; $i > 0; $i--) {
-        if ($numbber % 2 == 0) {
-            $numbber = $numbber + $operation1;
+        if ($i % 2 == 0) {
+            $number = $number + $operation1;
         } else {
-            $numbber = $numbber + $operation2;
+            $number = $number + $operation2;
         }
-        fwrite($myfile, $numbber."\n");
-        array_push($sequence, $numbber);
-        $y++;
+        array_push($sequence, $number);
     }
-    foreach ($sequence as $sigle){
-        fwrite($myfile, $sigle."\n");
-    }
-    $sequence_string = implode("A", $sequence);
-    fwrite($myfile, $sequence_string."\n");
+    $sequence_string = implode("|", $sequence);
     $solution = end($sequence);
-    if ($y % 2 == 0) {
-        $additional_solution = $solution + $operation1;
-       
-    } else {
-        $additional_solution = $solution + $operation2;
+    $additional_solution = $solution + (($length % 2 == 0) ? $operation1 : $operation2);
+    if ($additional_solution> $max_ergebniss){
+        return generateLevel2Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss);
     }
-    fwrite($myfile, $additional_solution."\n");
-    fwrite($myfile, $sequence_string."\n");
-    return array($sequence_string."A?A", $additional_solution, $dif_lvl, $operation1, $operation2);
-    fclose($myfile);
+    return [$sequence_string . "|?|", $additional_solution, $difficulty_level, $operation1, $operation2];
 }
 
 
-function generateLevel2Task() {
-    // to be implemented
+function generateLevel2Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss) {
+    $round++;
+    $sequence = array();
+    $after_witch = array();
+    $number = rand($min_Start_Zahl, $max_Start_Zahl);
+    $operation1 = rand($min_operationszahl, $max_operationszahl);
+    $operation2 = rand($min_operationszahl, $max_operationszahl);
+    $gleich_unterschiedlich = rand(1, 2);
+    $verlche_version_vom_lvl = rand(1, 3);
+    if ($gleich_unterschiedlich == 1){
+        $operation1 = $operation2;
+    }
+    $length = rand(4, 6);
+    if ($operation1 == 0) {
+        $operation1 = 1;
+    }
+    if ($operation2 == 0) {
+        $operation2 = 1;
+    }
+    $after_witch = switch_multi($number, $verlche_version_vom_lvl, $length, $operation1, $operation2);
+    $number = $after_witch[1];
+    $length = 1;
+    $sequence = $after_witch[0];
+    $sequence_string = implode("|", $sequence);
+    $additional_solution_array = switch_multi($number, $verlche_version_vom_lvl, $length, $operation1, $operation2);
+    $additional_solution = $after_witch[1];
+    if ($additional_solution> $max_ergebniss){
+        return generateLevel2Task($difficulty_level, $round, $max_operationszahl, $min_operationszahl, $max_Start_Zahl, $min_Start_Zahl, $max_ergebniss);
+    }
+    return array($sequence_string."|?|", $additional_solution, $difficulty_level, $operation1, $operation2);
 }
 
+function switch_multi($number, $verlche_version_vom_lvl, $length, $operation1, $operation2){
+    $sequence = array();
+    switch ($verlche_version_vom_lvl) {
+        case 1:
+            for ($i = $length; $i > 0; $i--) {
+                if ($i % 2 == 0) {
+                    $number = $number * $operation1;
+                } else {
+                    $number = $number * $operation2;
+                }
+                array_push($sequence, $number);
+            }
+            break;
+        case 2:
+            for ($i = $length; $i > 0; $i--) {
+                if ($i % 2 == 0) {
+                    $number = $number * $operation1;
+                } else {
+                    $number = $number + $operation2;
+                }
+                array_push($sequence, $number);
+            }
+            break;
+        case 3:
+            for ($i = $length; $i > 0; $i--) {
+                if ($i % 2 == 0) {
+                    $number = $number + $operation1;
+                } else {
+                    $number = $number + $operation2;
+                }
+                array_push($sequence, $number);
+            }
+            break;
+        case 4:
+            for ($i = $length; $i > 0; $i--) {
+                if ($i % 2 == 0) {
+                    $number = $number * $operation1;
+                } else {
+                    $number = $number * $operation2;
+                }
+                array_push($sequence, $number); 
+            }
+            break;
+    }
+    
+    $return_array= array($sequence, $number);
+    return $return_array;
+}
 function generateLevel3Task() {
     // to be implemented
 }
 
+?>
